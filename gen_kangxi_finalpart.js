03e312f540a1cfd6184b2144090dedbd6d118f19
yapcheahshen <yapcheahshen@gmail.com>
2014-10-18 15:00:00 +0800

    get final part ok

--- gen_kangxi_finalpart.js
+++ gen_kangxi_finalpart.js
@@ -3,12 +3,28 @@ var tokenize=require("./tokenize");
 var chise_ids=require("./chise_ids");
 var kangxi_wordhead=require("./kangxi_wordhead");
 var out=[];
-var findids=function(wh) {
+
+var getparts=function(wh) {
 	var ids=chise_ids[wh];
 	if (!ids) return;
-	ids=ids.replace(/[\u2ff0-\u2fff]/g,""); //remove idc
-	var tokens=tokenize(ids).tokens;
-	out.push('"'+wh+'":'+JSON.stringify(tokens));
+	parts=ids.replace(/[\u2ff0-\u2fff]/g,""); //remove idc
+	var parts=tokenize(parts).tokens;
+	return parts;
+}
+var findids=function(wh) {
+	var parts=getparts(wh);
+	if (!parts) return;
+	var finalparts=[];
+	while (parts.length) {
+		var part=parts.shift();
+		var childparts=getparts(part);
+		if (!childparts||childparts.length==1) {
+			finalparts.push(part);
+		} else {
+			parts=parts.concat(childparts);
+		}
+	}
+	out.push('"'+wh+'":'+JSON.stringify(finalparts));
 }
 kangxi_wordhead.forEach(findids);
 fs.writeFileSync("kangxi_finalpart.js","module.exports={"+out.join(",\n")+"}","utf8");
