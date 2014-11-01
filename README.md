fb5a367eec3dcb801e9d93f9a0d013c8ef91a754
samsuanchen <samsuanchen@gmail.com>
2014-11-01 16:21:32 +0800

    分析無碼部件

--- README.md
+++ README.md
@@ -1,37 +1,85 @@
 kangxidecompose
 ===============
 
-C:\dev2014>git clone https://github.com/yapcheahshen/kangxidecompose
 
-C:\dev2014>git clone https://github.com/ksanaforge/kangxizidian
+前置處理
+-------
 
-C:\dev2014/kangxizidian>npm install request
+dev2014>git clone https://github.com/yapcheahshen/kangxidecompose
 
-C:\dev2014/kangxizidian>node download_chise.js
+dev2014>git clone https://github.com/ksanaforge/kangxizidian
 
-C:\dev2014>git clone  https://github.com/ksanaforge/kangxizidian
+dev2014/kangxizidian>npm install request
 
-C:\dev2014/kangxizidian>node gen_kangxi_wordhead.js
+dev2014/kangxizidian>node download\_chise.js
 
-C:\dev2014/kangxizidian>node gen_chise_ids.js
+dev2014>git clone https://github.com/ksanaforge/kangxizidian
 
-C:\dev2014/kangxizidian>node gen_kangxi_finalpart.js
+dev2014/kangxizidian>node gen\_kangxi\_wordhead.js
 
-㐀 [U+3400](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=3400)	~ 䶵 [U+4DB5](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=4DB5)	unicode ext-a 
+dev2014/kangxizidian>node gen\_chise\_ids.js
 
-一 [U+4E00](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=4E00)	~ 龥 [U+9FA5](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=9FA5)	unicode CJK 中日韓漢字內碼
+dev2014/kangxizidian>node gen\_kangxi\_finalpart.js
 
-𠀀 [U+20000](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=20000)	~ 𪛖 [U+2A6D6](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=2A6D6)	unicode ext-b 
+後續處理
+-------
 
-歸納目前所寫的 js 程式檔如下:
+dev2014/kangxizidian>node gen\_u2b\_b2u.js
 
-1. gen_u2b_b2u.js 用以從 Unihan_OtherMappings.txt 產生 u2b.js 及 b2u.js 
+	generate u2b.s and b2u.js
 
-2. gen_kangxi_finalpart1.js + kangxi_finalpart.js (原字頭分析表) + u2b.js (unicode 轉 big5)
+dev2014/kangxizidian>node gen\_kangxi\_finalpart1.js (常用字)
+
+	generate kangxi_finalpart1.js
+
+dev2014/kangxizidian>node gen\_kangxi\_finalpart2.js (常用字)
+
+	產生 kangxi_finalpart2.csv (12531 詞頭拆分)
+	產生 kangxi_part2.csv (431 部件排序)
+
+dev2014/kangxizidian>node gen\_kangxi\_finalpart3.js (完整)
+
+	產生 kangxi_finalpart3.csv (46844 詞頭拆分)
+	產生 kangxi_part3.csv (1892 部件排序)
+	產生 kangxi_unknownFreq3.csv (1260 無碼部件分析)
+	
+dev2014/kangxizidian>gulp qunit --js=gen\_kangxi\_finalpart3.js
+
+	除錯追蹤 環境起動
+
+相當處理包括資訊
+--------------
+
+CJK Radicals Supplement ([⺀](http://www.unicode.org/charts/PDF/U2E80.pdf) ~ [⻳](http://www.unicode.org/charts/PDF/U2E80.pdf))
+
+	U+2E80 ~ U+2EF3
+
+unicodeExt-a ([㐀](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=3400) ~ [䶵](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=4DB5))
+
+	U+3400 ~ U+4DB5	
+
+unicode CJK 中日韓漢字內碼 ([一](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=4E00) ~ [龥](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=9FA5))
+
+	U+4E00 ~ U+9FA5	
+
+unicode ext-b ([𠀀](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=20000) ~ [𪛖](http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=2A6D6))
+
+	U+20000 ~ U+2A6D6	 
+
+Variation Selectors Supplement
+
+	U+E0100 ~ U+E01EF 
+
+歸納目前所寫的 js 程式檔
+----------------------
+
+1. gen\_u2b\_b2u.js 用以從 Unihan_OtherMappings.txt 產生 u2b.js 及 b2u.js 
+
+2. gen\_kangxi\_finalpart1.js + kangxi\_finalpart.js (原字頭拆分表) + u2b.js (unicode 轉 big5)
     用以產生 kangxi_finalpart1.js (保留big5常用字)
 
-3. gen_kangxi_finalpart2.js + kangxi_finalpart1.js (big5常用字) + u2b.js (unicode 轉 big5)
-    用以產生 kangxi_finalpart2.csv (12531 字頭分析) + kangxi_part2.csv (431 常用字部件排序)
+3. gen\_kangxi\_finalpart2.js + kangxi\_finalpart1.js (big5常用字) + u2b.js (unicode 轉 big5)
+    用以產生 kangxi_finalpart2.csv (12531 字頭拆分) + kangxi_part2.csv (431 部件排序)
 
-4. gen_kangxi_finalpart3.js + kangxi_finalpart.js (原字頭分析表) + u2b.js (unicode 轉 big5)
-    用以產生 kangxi_finalpart3.csv (46844 字頭分析) + kangxi_part3.csv (631 常用字部件排序)
+4. gen\_kangxi\_finalpart3.js + kangxi\_finalpart.js (原字頭拆分表) + u2b.js (unicode 轉 big5)
+    用以產生 kangxi_finalpart3.csv (46844 字頭拆分) + kangxi_part3.csv (1892 部件排序) + kangxi_partFreq3.csv (1260 部件排序) 
--- gen_kangxi_finalpart3.js
+++ gen_kangxi_finalpart3.js
@@ -1,52 +1,102 @@
-var fs=require("fs"); // 產生 康熙字典 末級部件表 及 常用字拆解分析表
-	
-function decodeUnicode(str) { // handling ext-b surrogate pairs as well
-    var r = [], i = 0;
+debugger
+var fs=require("fs");	// 產生 康熙字典 字頭拆解分析表 及 末級部件排序表
+function hex(n,m){		// 將整數轉成 16 進制字串 (前置 0 補足 m 碼)
+	if(!n) return '';
+	m=m||5;									// 預設 n 為 整數 0, m 為 5
+	if(typeof n!=='string')
+		n=n.toString(16);					// 整數 n 轉成 16 進制字串 t
+	return '0000'.substr(0,m-n.length)+n;	// 前置 0 補足 m 碼
+}
+function utf16Char(codeUnit){				// 將整數轉成 utf16 字符
+	return String.fromCharCode(codeUnit);
+}
+function deUnicode(str) { // 將 utf16 字串 轉成 unicode 整數陣列
+// extract unicodes (integer array) from given utf16 string
+    var charCodes = [], i = 0;
     while(i < str.length) {
-        var chr = str.charCodeAt(i++);
-        if(chr >= 0xD800 && chr <= 0xDBFF) {
-            // surrogate pair
-            var low = str.charCodeAt(i++);
-            r.push(0x10000 + ((chr - 0xD800) << 10) | (low - 0xDC00));
-        } else {
-            // ordinary character
-            r.push(chr);
-        }
+        var charCode = str.charCodeAt(i++);
+        if(charCode<0xD800 || charCode>0xDBFF) // ordinary character or user defined
+            charCodes.push(charCode);
+        else // extract surrogate pair (get upper bits and additional lower 10 bits)
+            charCodes.push(0x10000 + ((charCode-0xD800)<<10) | (str.charCodeAt(i++)-0xDC00));
     }
-    return r;
+    return charCodes; // integer array (each value < 0x30000)
+}
+function enUnicode(unicodes){ // 將 unicode 整數陣列 轉成 utf16 字串
+// encode utf16 string from given unicodes (integer array)
+    var TEN_BITS = parseInt('1111111111', 2);
+    return unicodes.map(function(codePoint){
+	    if (codePoint<0x10000)	// ordinary (value<0xd800) or user defined (value>=0xe000)
+	        return utf16Char(codePoint);
+	    codePoint -= 0x10000;	// encodeing surrogate pair
+	    return utf16Char(0xD800+(codePoint>>10)) + utf16Char(0xDC00+(codePoint&TEN_BITS));
+    }).join('');
+}
+var P={};					// 部件引用頻次 資訊
+function countPart(k,p){		// 部件引用頻次 累計
+	var o=P[p];
+	if(!o)
+		P[p]=o=[];
+//	if(o.indexOf(k)<0)
+		o.push(k);
 }
 var kangxi_finalpart=require("./kangxi_finalpart.js");
-console.log('kangxi_finalpart',Object.keys(kangxi_finalpart).length,'wordheads');
+console.log('載入 康熙詞頭拆分前置資訊 kangxi_finalpart.js',Object.keys(kangxi_finalpart).length);
 var u2b=require("./u2b.js");
-var out=[],P={},K=Object.keys(kangxi_finalpart);
-var err=0;
-for(var i=0; i<K.length; i++){ var k=K[i];
-	var o=[k,decodeUnicode(k).map(function(c){
-		//if(err<300&&c<0x3400)
-		//	console.log(err++,i,'字 =',k,'字碼= U+',c.toString(16),'<0x3400');
-		return 'U+'+c.toString(16);
-	}).join(',')];
-	kangxi_finalpart[k].filter(function(p){return decodeUnicode(p)<0x30000;}).forEach(function(p){
-		if(p.match(/&.+?;/)) p='?',u='';
-		else {
-			P[p]=P[p]||0, P[p]++;
-			u=decodeUnicode(p).map(function(c){
-				//if(err<300&&c<0x3400)
-				//	console.log(err++,i,'部 =',p,'部碼= U+',c.toString(16),'<0x3400');
-				return 'U+'+c.toString(16);
-			}).join(',')
-		}
-		o.push(p),o.push(u);
+console.log('載入 unicode 轉 big5 資訊 u2b.js',Object.keys(u2b).length);
+var K=Object.keys(kangxi_finalpart);		// 康熙詞頭陣列
+var unknown={names:[''],code:[''],length:0};// 無對應 unicode 部件 資訊
+var unknownInd;								// 無對應 unicode 部件 序號 與 名稱
+var out=[];									// 輸出準備陣列
+for(var i=0; i<K.length; i++){
+	var k=K[i];								// 詞頭 字串
+	var u=deUnicode(k)[0];					// 詞頭 unicode	整數值
+	var b=u2b[u];							// 詞頭 big5	對應值
+	u='u'+hex(u);							// 詞頭 unicode	碼
+	b=b?('b'+hex(b)):'';					// 詞頭 big5	碼 若無對應 unicode 碼 就設為空字串
+	var o=[k,u,b];							// 當前資料列 準備輸出 前三欄 詞頭,unicode碼,big5碼
+	var parts=kangxi_finalpart[k];			// 部件字串 陣列
+	var e;									// 部件字符 
+	parts=parts.filter(function(p){
+		return deUnicode(p)[0]<0x30000;		// 忽略 Variation Selectors U+E0100 ~ U+E01EF
 	});
-	out.push(o.map(function(p){return '"'+p+'"'}).join(','));
+	parts.forEach(function(p){
+		if(p=='?'||p.match(/&.+?;/)){		// 部件字串 若無對應 unicode 碼
+			unknownInd=unknown[p];			// 取 無碼部件 序號
+			if(!unknownInd){				// 序號為 0 表示 未處理過
+				unknown[p]=unknownInd=++unknown.length; // 自動產生 無碼部件 序號
+				unknown.names[unknownInd]='?'+unknownInd.toString(16);
+				unknown.code [unknownInd]=p;
+			}
+			var q=unknown.names[unknownInd];	// 無碼部件 名稱     ?1 ~   ?4e9
+			o.push(q), countPart(k,q);			// 無碼部件 頻次 累計
+			o.push('q'+hex(unknownInd));		// 無碼部件 代碼 u00001 ~ u004e9
+		} else {
+			p=deUnicode(p);						// 部件字串 轉為 部件字符 unicode 整數陣列
+			p.forEach(function(c){ 				// 部件字符 unicode 整數值 c
+				e=enUnicode([c]);				// 部件字符 e
+				o.push(e), countPart(k,e);		// 部件字符 頻次 累計
+				o.push('u'+hex(c));			// 部件字符 代碼 U02e81 ~ U2f97a
+			})
+		}
+	});
+	out.push(o.map(function(x){
+		return '"'+x+'"';
+	}).join(','));
 };
-console.log('kangxi_finalpart3.csv',out.length,'wordheads');
-fs.writeFileSync("kangxi_finalpart3.csv",out.join(",\n"),'utf8');
-var p=Object.keys(P);
-out=p.map(function(c,j){
-	return [c,decodeUnicode(c)[0]];
-}).sort(function(a,b){return a[1]-b[1];}).map(function(a){
-	return '"'+a[0]+'","U+'+a[1].toString(16)+'"'
+console.log('詞頭拆分總數',out.length);
+fs.writeFileSync("kangxi_finalpart3.csv",out.join("\n"),'utf8');
+out=Object.keys(P).map(function(e){var x;
+	if(e.substr(0,1)==='?')
+		c='q'+hex(parseInt(e.substr(1),16));
+	else
+		c='u'+hex(deUnicode(e)[0]);
+	return '"'+c+'","'+e+'","'+P[e].join(',')+'"';
 });
-console.log('末級部件總數',out.length);
-fs.writeFileSync("kangxi_part3.csv",out.join(",\n"),'utf8');
+console.log('部件排序總數',out.length);
+fs.writeFileSync("kangxi_part3.csv",out.sort().join("\n"),'utf8');
+console.log('無碼部件總數',unknown.length);
+out=unknown.names.slice(1).map(function(e){
+	return '0x'+hex(P[e].length,3)+',"'+e+'","'+P[e].join(',')+'"';
+}).sort();
+fs.writeFileSync("kangxi_unknownFreq3.csv",out.join("\n"),'utf8');
